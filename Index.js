deb929ba9513927e9ebf9fb2474b992dc8125388
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-09 16:59:41 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,36 @@
+// console.log('Hello Winc Academy')
+
+// let name = 'Richard'
+// console.log('Richard')
+
+// let a = 4;
+// let b = 4;
+// let c = a + b;
+// alert(c);
+
+// let a = 4;
+// let b = 4;
+// let c = a * b;
+// console.log(c);
+
+// let a = 16;
+// let b = 4;
+// let c = a / b;
+// alert(c);
+
+// let a = 20;
+// let b = 100;
+// let c = a % b;
+// console.log(c);
+
+// let a = 20;
+// let b = 60;
+// let c = a - b;
+// alert(c);
+
+// let age = 47;
+let age = 'fourtyseven';
+console.log(typeof age)
+
+
+
